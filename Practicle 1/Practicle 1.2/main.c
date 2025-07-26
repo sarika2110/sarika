68def0071abb464dab2dabb532a6eb186a107b96
sarika2110 <sarikasingh27102007@gmail.com>
2025-07-26 14:43:08 +0530

    Add files via upload

---
+++ Practicle 1/Practicle 1.2/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    float P,R,T,SI;
+
+    printf("ENTER PRINCIPLE AMOUNT: \n");
+    scanf("%f" , &P);
+
+    printf("ENTER RATE OF INTREST: \n");
+    scanf("%f" , &R);
+
+    printf("ENTER TOTAL INTREST (IN  YEARS): \n");
+    scanf("%f" , &T);
+
+    SI = (P*R*T) / 100;
+
+    printf("SIMPEL INTREST = %.2f \n", SI);
+
+    return 0;
+}
